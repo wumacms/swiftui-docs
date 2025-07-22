a919a939d9c047468ab613b7f7d0d0df8f9dda65
wumacms <wumacms@qq.com>
2025-07-22 20:35:32 +0800

    页面标题组件

---
+++ views/components/PageTitle.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useRoute } from 'vitepress'
+
+const route = useRoute()
+
+// 从路由中解析页面标题，并进行URL解码（例如从路径"/abc.html"中解析出"abc"）
+const webTitle = decodeURIComponent(route.path.split('/').pop().replace(/\.html$/, ''));
+
+</script>
+
+<template>
+    <span>{{ webTitle }}</span>
+</template>
